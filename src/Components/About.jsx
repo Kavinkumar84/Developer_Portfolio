799ec5dfdda9a9c25fe6e6e6a8a7c58d6423fb76
Kavinkumar84 <kavinkumar200448@gmail.com>
2025-11-08 14:57:13 +0530

    About Mobile Responsive

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                 <Col lg={6}>
                 <div className="About_Left_Container">
                     <div className="About_Left_txt1">WHO I AM?</div>
-                    <div className="About_Left_txt2">My name is KAVIN KUMAR. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.</div>
+                    <div className="About_Left_txt2">My name is <span style={{ color: "#E60076" }}>KAVIN KUMAR</span>. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.</div>
                 </div>
                 </Col>
                 <Col lg={6}>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import "./Home.css";
 import { Col, Row } from "react-bootstrap";
+
 import {
   FaDownload,
   FaGithub,
@@ -11,8 +11,7 @@ import {
 import { SiGeeksforgeeks, SiLeetcode } from "react-icons/si";
 const Home = () => {
 
-  let bracket_open="{"
-  let bracket_close="}"
+
   return (
     <div className="Home">
       <div className="Home_bg">
@@ -50,9 +49,9 @@ const Home = () => {
                     Hello,
                     <br />
                     This is
-                    <span style={{ color: "#E60076" }}> KAVIN KUMAR</span> , I'm
+                    <span className=""style={{ color: "#E60076" }}> KAVIN KUMAR</span> , I'm
                     a Professional
-                    <span style={{ color: "#16f2b3" }}> Web Developer</span>.
+                    <span style={{ color: "#16f2b3" }}> Web Developer.</span>
                   </div>
                   <div className="Social_Icons">
                     <a href="https://github.com/Kavinkumar84" target="_blank">
@@ -95,29 +94,31 @@ const Home = () => {
               </Col>
               <Col lg={6} md={12} sm={12}>
                 <div className="Home_Right">
-                  <div class="card">
-                    <div class="tools">
-                      <div class="circle">
-                        <span class="yellow box"></span>
+                  <div className="card">
+                    <div className="tools">
+                      <div className="circle">
+                        <span className="yellow box"></span>
                       </div>
-                      <div class="circle">
-                        <span class="green box"></span>
+                      <div className="circle">
+                        <span className="green box"></span>
                       </div>
-                      <div class="circle">
-                        <span class="red box"></span>
+                      <div className="circle">
+                        <span className="red box"></span>
                       </div>
                     </div>
-                    <div class="card__content">
+                    <div className="card__content">
                       <div className="Home_card_txt">
-                        <div><span style={{color : "#e60076"}}>const</span> Developer<span style={{color : "#e60076"}}> = </span>{bracket_open}</div>
+                        <div><span style={{color : "#e60076"}}>const</span> Developer<span style={{color : "#e60076"}}> = </span>{"{"}</div>
                         <div className="Home_card_txt1">
                           <div className="">name:'<span className="yellow_code_txt">Kavin Kumar</span>',</div>
-                          <div className="">title:['<span className="yellow_code_txt">MERN Stack Developer</span>', '<span className="yellow_code_txt">React.js Developer</span>', '<span className="yellow_code_txt">Node.js Developer</span>'],</div>
-                          <div className="">skills:['<span className="yellow_code_txt">Java</span>','<span className="yellow_code_txt">HTML</span>', '<span className="yellow_code_txt">CSS</span>', '<span className="yellow_code_txt">JavaScript</span>', '<span className="yellow_code_txt">React</span>', '<span className="yellow_code_txt">NodeJS</span>', '<span className="yellow_code_txt">Express</span>', '<span className="yellow_code_txt">MangoDB</span>', '<span className="yellow_code_txt">MySql</span>'],</div>
+                          <div className="">title:'<span className="yellow_code_txt">MERN Stack Developer</span> | <span className="yellow_code_txt">React.js Developer</span> | <span className="yellow_code_txt">Node.js Developer</span>',</div>
+                          <div className="">skills:[
+                            <div className="Home_card_txt1">'<span className="yellow_code_txt">Java</span>','<span className="yellow_code_txt">HTML</span>', '<span className="yellow_code_txt">CSS</span>', '<span className="yellow_code_txt">JavaScript</span>', '<span className="yellow_code_txt">React</span>', '<span className="yellow_code_txt">NodeJS</span>', '<span className="yellow_code_txt">Express</span>', '<span className="yellow_code_txt">MangoDB</span>', '<span className="yellow_code_txt">MySql</span>'</div>
+                            ],</div>
                           <div className="">hardWorker: <span className="orrange_code_txt">true</span>,</div>
                           <div className="">quickLearner: <span className="orrange_code_txt">true</span>,</div>
                           <div className="">problemSolver: <span className="orrange_code_txt">true</span>,</div>
-                          <div className=""><span className="green_code_txt">hireable: </span><span className="orrange_code_txt">function</span>() {bracket_open} </div>
+                          <div className=""><span className="green_code_txt">hireable: </span><span className="orrange_code_txt">function</span>() {"{"} </div>
                           <div className="Home_card_txt1">
                             <div className=""><span className="orrange_code_txt">return </span>(</div>
                             <div className="Home_card_txt1">
