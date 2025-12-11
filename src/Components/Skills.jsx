ddd96a4d369f520d10d2ff5bd8d3109818e2fd8c
Kavinkumar84 <kavinkumar200448@gmail.com>
2025-12-11 15:14:16 +0530

    Skill Component Added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,31 +1,54 @@
 import React from "react";
 import "./Skills.css";
+import Java from '../assets/Java.png'
+import HTML5 from '../assets/HTML5.png'
+import CSS3 from '../assets/CSS3.png'
+import Bootstrap from '../assets/Bootstrap.png'
+import JavaScript from '../assets/JS.png'
+import Reactjs from '../assets/React.png'
+import Vite from '../assets/Vite.png'
+import Node from '../assets/Node.png'
+import ExpressJs from '../assets/ExpressJs.png'
+import MongoDB from '../assets/MongoDB.png'
+import MySQL from '../assets/MySQL.png'
+import PostgresSQL from '../assets/PostgresSQL.png'
+import Firebase from '../assets/Firebase.png'
+import Postman from '../assets/Postman.png'
+import EmbeddedC from '../assets/EmbeddedC.png'
+import Arduino from '../assets/Arduino.png'
+import Git from '../assets/Git.png'
+import VSCode from '../assets/VSCode.png'
+import Eclipse from '../assets/Eclipse.png'
+import Intellij from '../assets/Intellij.png'
+import Netlify from '../assets/Netlify.png'
+import Vercel from '../assets/Vercel.png'
 import Marquee from "react-fast-marquee";
 const Skills = () => {
   const skills = [
-    { img: "src/assets/Java.png", title: "Java" },
-    { img: "src/assets/HTML5.png", title: "HTML5" },
-    { img: "src/assets/CSS3.png", title: "CSS3" },
-    { img: "src/assets/Bootstrap.png", title: "Bootstrap" },
-    { img: "src/assets/JS.png", title: "JavaScript" },
-    { img: "src/assets/React.png", title: "React" },
-    { img: "src/assets/Vite.png", title: "Vite" },
-    { img: "src/assets/Node.png", title: "Node.js" },
-    { img: "src/assets/ExpressJS.png", title: "Express.js" },
-    { img: "src/assets/MongoDB.png", title: "MongoDB" },
-    { img: "src/assets/MySQL.png", title: "MySQL" },
-    { img: "src/assets/PostgresSQL.png", title: "PostgreSQL" },
-    { img: "src/assets/Firebase.png", title: "Firebase" },
-    { img: "src/assets/Postman.png", title: "Postman" },
-    { img: "src/assets/EmbeddedC.png", title: "Embedded C" },
-    { img: "src/assets/Arduino.png", title: "Arduino" },
-    { img: "src/assets/Git.png", title: "Git" },
-    { img: "src/assets/VSCode.png", title: "VS Code" },
-    { img: "src/assets/Eclipse.png", title: "Eclipse" },
-    { img: "src/assets/Intellij.png", title: "IntelliJ" },
-    { img: "src/assets/Netlify.png", title: "Netlify" },
-    { img: "src/assets/Vercel.png", title: "Vercel" },
-  ];
+  { img: Java, title: "Java" },
+  { img: HTML5, title: "HTML5" },
+  { img: CSS3, title: "CSS3" },
+  { img: Bootstrap, title: "Bootstrap" },
+  { img: JavaScript, title: "JavaScript" },
+  { img: Reactjs, title: "React" },
+  { img: Vite, title: "Vite" },
+  { img: Node, title: "Node.js" },
+  { img: ExpressJs, title: "Express.js" },
+  { img: MongoDB, title: "MongoDB" },
+  { img: MySQL, title: "MySQL" },
+  { img: PostgresSQL, title: "PostgreSQL" },
+  { img: Firebase, title: "Firebase" },
+  { img: Postman, title: "Postman" },
+  { img: EmbeddedC, title: "Embedded C" },
+  { img: Arduino, title: "Arduino" },
+  { img: Git, title: "Git" },
+  { img: VSCode, title: "VS Code" },
+  { img: Eclipse, title: "Eclipse" },
+  { img: Intellij, title: "IntelliJ" },
+  { img: Netlify, title: "Netlify" },
+  { img: Vercel, title: "Vercel" },
+];
+
   return (
     <div id="Skills">
       <div className="Skill_whole">
